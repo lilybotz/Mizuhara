39b169c9dff44bd60780fa27a472732c764dc3a0
Jennie baby <108253179+lilybotz@users.noreply.github.com>
2022-09-19 15:07:42 -1200

    Update config.js

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const chalk = require("chalk");
 // Other
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983";
+  gg = "94726083178";
 }
 
-global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "none";
+global.owner = gg.split("ME");
+global.mongodb = process.env.MONGODB || "mongodb+srv://cherry:cherry@cluster0.odvy3r2.mongodb.net/?retryWrites=true&w=majority";
 global.pgdb = process.env.DATABASE_URL || "none";
 global.port = process.env.PORT || 8080;
-(global.packname = process.env.PACKNAME || "kaguya"),
-  (global.prefix = process.env.PREFIX || "-"),
-  (global.author = process.env.AUTHOR || "ARUS"),
+(global.packname = process.env.PACKNAME || "Lakshan"),
+  (global.prefix = process.env.PREFIX || "."),
+  (global.author = process.env.AUTHOR || "xyz"),
   (global.sessionName = "arus"),
-  (global.name = process.env.NAME || "Mizuhara"),
+  (global.name = process.env.NAME || "MITSUHA"),
   (global.thumb = fs.readFileSync("./trash/info.jpeg"));
 
 let file = require.resolve(__filename);
